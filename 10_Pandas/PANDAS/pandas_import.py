0384a34682b81587ede93bfafcdd630c9faaa1d0
Dellamorte383 <pr@exptime.ru>
2022-05-18 23:10:37 +0300

    update

--- 10_Pandas/PANDAS/pandas_import.py
+++ 10_Pandas/PANDAS/pandas_import.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
-data = pd.read_csv('https://raw.githubusercontent.com/esabunor/MLWorkspace/master/melb_data.csv')
-print(data)
+#data = pd.read_csv('https://raw.githubusercontent.com/esabunor/MLWorkspace/master/melb_data.csv')
+#print(data)
+
+
+
+
+melb_data = pd.read_csv('data/melb_data.csv', sep=',')
